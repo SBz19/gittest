1f39235e9e579805a8cd06a61a60188741a94646
S B <sbthegamer19@gmail.com>
2020-06-08 17:23:43 +0800

    commitf

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class main {
  
     public static void main(String[] args) {
-        System.out.println("Hello, Github");
+        System.out.println("Hello, Github. I am a dude.");
     }
 }
